f6bdcf2e9372cac1a82ee5153569470fb72c5f56
ilakkiyan-uruthireswaran-ics4u1-3-25 <Ilakkiyan.uruthireswaran@student.tdsb.on.ca>
2025-04-09 03:14:28 +0000

    passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,35 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static int sumOfDigits(int x) {
+		if (x == 0) {
+			return 0;
+		}
 
+		if (x < 0) {
+			x *= -1;
+		}
+
+		return (x % 10) + sumOfDigits(x / 10);
+	}
+
+	public static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		}
+
+		if (n < 0) {
+			return 0;
+		}
+
+		return factorial(n-1)*n;
+	}
+
+	public static void main(String args[]) {
+		int result = sumOfDigits(222);
+		System.out.println(result);
+
+		int result2 = factorial(7);
+		System.out.println(result2);
 	}
 
 	
